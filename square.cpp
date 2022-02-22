e956e79d2eb83a9ebe700091020c3ac03ecc474c
emilyvekariya <98161797+emilyvekariya@users.noreply.github.com>
2022-02-22 23:28:10 +0530

    Add files via upload

---
+++ square.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+int i;
+cout << "this is the output. " <<endl;
+cout << "enter the number: ";
+cin >> i;
+cout << i << " square is: " << i*i <<endl;
+return 0;
+}
