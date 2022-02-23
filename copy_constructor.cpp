77c8eb396817c6fa9c72e4ca63e0914628ecdcc8
emilyvekariya <98161797+emilyvekariya@users.noreply.github.com>
2022-02-23 17:56:18 +0530

    Add files via upload

---
+++ copy_constructor.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+class hello
+{
+    private:
+    int x, y;  
+   
+    public:
+    hello(int x1, int y1)
+    {
+        x = x1;
+        y = y1;
+    }
+   
+    /* Copy constructor */
+    hello(const hello &a)
+    {
+        x = a.x;
+        y = a.y;
+    }
+   
+    void display()
+    {
+        cout<<x<<" "<<y<<endl;
+    }
+};
+
+int main()
+{
+    hello obj1(13,15);     // Normal constructor
+    hello obj2 = obj1;      // Copy constructor
+    cout<<"Normal constructor : ";
+    obj1.display();
+    cout<<"Copy constructor : ";
+    obj2.display();
+
+    return 0;
+}
