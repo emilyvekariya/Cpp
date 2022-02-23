36e4fbc930252986c376cd92f331034a477d2da7
emilyvekariya <98161797+emilyvekariya@users.noreply.github.com>
2022-02-23 15:24:43 +0530

    Update operator_overloading_class.cpp

--- operator_overloading_class.cpp
+++ operator_overloading_class.cpp
@@ -15,6 +15,9 @@ class class1{
         }
 
         friend class1 operator+(class1 c1, class2 c2);
+        friend class1 operator+(class1 c1, class1 c2);
+        friend class1 operator+(class2 c1, class2 c2);
+        friend class1 operator+(class2 c1, class1 c2);
 };
 
 class class2{
@@ -31,6 +34,9 @@ class class2{
             cout<<"a is: "<<a<<" and b is: "<<b<<endl;
         }
         friend class1 operator+(class1 c1, class2 c2);
+        friend class1 operator+(class1 c1, class1 c2);
+        friend class1 operator+(class2 c1, class2 c2);
+        friend class1 operator+(class2 c1, class1 c2);
 };
 
 class1 operator+(class1 c1, class2 c2){
@@ -39,14 +45,41 @@ class1 operator+(class1 c1, class2 c2){
     temp.b = c1.b + c2.b;
     return temp;
 }
+class1 operator+(class1 c1, class1 c2){
+    class1 temp;
+    temp.a = c1.a + c2.a;
+    temp.b = c1.b + c2.b;
+    return temp;
+}
+class1 operator+(class2 c1, class2 c2){
+    class1 temp;
+    temp.a = c1.a + c2.a;
+    temp.b = c1.b + c2.b;
+    return temp;
+}
+class1 operator+(class2 c1, class1 c2){
+    class1 temp;
+    temp.a = c1.a + c2.a;
+    temp.b = c1.b + c2.b;
+    return temp;
+}
 
 int main(){
+    cout<<"object 1 values: "<<endl;
     class1 c1(10,20);
     c1.getData();
 
-    class2 c2(30,40);
+    class1 c2(20,40);
     c2.getData();
 
+    cout<<"object 2 values: "<<endl;
+    class2 d2(30,40);
+    d2.getData();
+
+    class2 d1(10,60);
+    d1.getData();
+
+    cout<<"sum value: "<<endl;
     class1 c3 = c1 + c2; //first method to friend function invoke operator overloading
     // class1 c3 = operator+(c1, c2); //second method to friend function invoke operator overloading
     c3.getData();
